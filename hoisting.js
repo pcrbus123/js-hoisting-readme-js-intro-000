057bb8d9cd61db6d2d502551855c9d46011b7799
Pnina Breslav <pcrbus123@gmail.com>
2018-10-07 08:45:26 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,11 +1,10 @@
 function callMe() {
   var lyric = "maybe";
+  return lyric;
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
-  console.log("so call me");
-  return lyric;
-}
+  console.log("so call me");}
 
 
 function crazy() {
@@ -26,6 +25,6 @@ function sayMyName() {
   function sayMy() {
     var name = "Kristin";
     console.log(name);
-    
+
   }
 }
